f94765e44ee7c651749deb9e1febd88e072e4d51
belleblazer4life <70744391+belleblazer4life@users.noreply.github.com>
2020-12-08 18:49:39 +0100

    Small changes on alert button

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let obj=document.childNodes;
-let len= obj.length;
+let obj = document.childNodes;
+let len = obj.length;
 
-for(let i=0;i<len;i++){
-console.log(obj[i].childNodes);
+for (let i = 0; i < len; i++) {
+  console.log(obj[i].childNodes);
 }
+
+btn = document.getElementById('btn-alert');
+btn.addEventListener('click', (e) => {
+  alert('Hello belleblazer4life......\nYou clicked me!');
+});
